00f8badd2dbd43a9fc8dc73d5b539ebecf854805
Josipa Juricic <jjuricic1@pmfst.hr>
2020-10-25 20:53:19 +0100

    build

---
+++ build/precache-manifest.5972b9eaa02c7d44bff3bfbf096f364b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b47a7a2387f0475e54a635e2b6fb5d3c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1ba10854f2115deaa2ae",
+    "url": "/static/css/main.b7102fc7.chunk.css"
+  },
+  {
+    "revision": "07fa9e622bdd6a898738",
+    "url": "/static/js/2.7706df46.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.7706df46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ba10854f2115deaa2ae",
+    "url": "/static/js/main.ea28620b.chunk.js"
+  },
+  {
+    "revision": "922b17ff8652a18bce8a",
+    "url": "/static/js/runtime-main.426088c4.js"
+  }
+]);
